9a309197d3c9f05b8b912506db40c215cf2f62c0
JANVIER AUBRY Elyzio <elyziojanvieraubry@gmail.com>
2026-09-15 11:44:25 +0200

    :sparkles: ajout des sessions

--- website/app.py
+++ website/app.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from database import DatabaseManager    
+from database import DatabaseManager
+from functools import wraps
+from flask import abort
+
+def admin_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect(url_for('login'))
+        user = db.get_user_by_id(session['user_id'])
+        if not user or user [3] != 'admin':
+            abort(403)
+        return f(*args, **kwargs)
+    return wrapper
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a random secret key
@@ -13,6 +26,7 @@ def login():
         user = db.login(name, password)
         if user:
             session['user_id'] = user[0]
+            session['role'] = user[3]
             return redirect(url_for('home'))
         else:
             return render_template('login.html', error="Identifiants invalides"), 401
@@ -20,14 +34,14 @@ def login():
 
 @app.route('/home')
 def home():
-    if 'user_id' in session:
-        name_ = str(db.get_user_by_id(session['user_id'])[1])
-        users_list = db.get_users_list()
-        return render_template('home.html', name=name_, users=users_list)
-    else:
+    if 'user_id' not in session:
         return redirect(url_for('login'))
+    user = db.get_user_by_id(session['user_id'])
+    user_list = db.get_users_list() if user[3] == 'admin' else []
+    return render_template('home.html', name=user[1], role=user[3], users=user_list)
 
 @app.route('/add_user', methods=['POST'])
+@admin_required
 def add_user():
     if request.method == 'POST':
         name = request.form['name']
@@ -41,6 +55,7 @@ def add_user():
     return render_template('home.html')
 
 @app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
+@admin_required
 def edit_user(user_id):
     if request.method == 'POST':
         name = request.form['name']
@@ -52,6 +67,7 @@ def edit_user(user_id):
     return render_template('edit_user.html', user=user)
 
 @app.route('/delete_user/<int:user_id>')
+@admin_required
 def delete_user(user_id):
     db.delete_user(user_id)
     return redirect(url_for('home'))
